017c860e500671d4051a6d1a8bd4dedb33f8dc54
Kevin Hernandez <kevinalbertohernandezgarcia@gmail.com>
2026-02-24 11:54:30 -0600

    Initial Commit

---
+++ BudgetBuddy/BudgetBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetBuddyApp.swift
+//  BudgetBuddy
+//
+//  Created by Kevin Hernandez Garcia on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct BudgetBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgetBuddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BudgetBuddy
+//
+//  Created by Kevin Hernandez Garcia on 2/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
